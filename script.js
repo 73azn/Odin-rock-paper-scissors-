cd8e0a6f4081159e313f7c7b712dc542e5bf5207
73azn <amerbinazim@gmail.com>
2024-08-05 23:46:52 +0300

    added scores and fully playable game

--- script.js
+++ script.js
@@ -10,20 +10,25 @@ function getHumanChoice(){
 }
 
 
-const AI_choice = getComputerChoice()
-const humanChoice = getHumanChoice()
-
+let AI_choice
+let humanChoice 
+let humanScore = 0;
+let AI_score = 0;
 function check(){
+    if(humanChoice == undefined || humanChoice>3){
+        console.log("invalid input")
+        return
+    }
     console.log(`the human are ${humanChoice} the AI are ${AI_choice}`)
     if(AI_choice == humanChoice){
         console.log("Tie")
         return
     }
-    
 
     if(humanChoice == arr[0]){
         if(AI_choice ==arr[1]){
             console.log("you lose and AI won")
+            AI_score++
             return
         }
        
@@ -31,6 +36,7 @@ function check(){
     if(humanChoice == arr[1]){
         if(AI_choice == arr[2]){
             console.log("you lose and AI won")
+            AI_score++
             return
         }
         
@@ -39,13 +45,19 @@ function check(){
     if(humanChoice == arr[2]){
         if(AI_choice == arr[0]){
             console.log("you lose and AI won")
+            AI_score++
             return
         }
     }
 
     console.log("you won and AI lose")
-    
+    humanScore++
+}
+console.log("best of 3")
+while(humanScore < 3 && AI_score <3){
+AI_choice = getComputerChoice()
+humanChoice = getHumanChoice()
+check()
+console.log(`human score ${humanScore} AI score ${AI_score}`)
 
 }
-
-check()
